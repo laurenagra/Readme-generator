9abeb29bb67c824498fbc278631cf0178e3418b5
Lauren A <laurenhaileyama@gmail.com>
2022-03-01 11:57:06 -0500

    update generate code, input questions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,8 +12,41 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  //licensing info missing 
   return `# ${data.title}
 
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Test Instructions](#test-instructions)
+  - [Contact Information](#contact-information)
+
+
+  ### Description
+  ${data.description}
+
+
+  ### Installation
+  ${data.installation}
+
+
+  ### Usage
+  ${data.usage}
+
+
+  ### Contribution
+  ${data.contribution}
+
+
+  ### Test-Instructions
+  ${data.tests}
+
+  ### Contact-Information
+[Github Profile](https://github.com/${data.username})
+${data.email}
+  
 `;
 }
 
